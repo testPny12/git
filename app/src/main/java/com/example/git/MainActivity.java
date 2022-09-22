d9709d88805b5457b7461e6c4becb8670051af5e
testpny12 <testpny1@gmail.com>
2022-09-22 20:47:28 +0500

    Test

--- app/src/main/java/com/example/git/MainActivity.java
+++ app/src/main/java/com/example/git/MainActivity.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    int i = 100;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
